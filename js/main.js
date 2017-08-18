a408153ec5a9eed96f38120019f94cc3e7d121d4
Chris <chansenwebdevanddesigns@gmail.comgit config --global user.email chansenwebdevanddesigns@gmail.com>
2017-08-17 23:37:46 -0700

    Initial commit

--- js/main.js
+++ js/main.js
@@ -5,28 +5,68 @@ $(document).ready(() => {
         e.preventDefault();
     });
 });
-// http://www.omdbapi.com/?apikey=ee3d33c2&?s
-//http://www.omdbapi.com/?apikey=ee3d33c2&?s
-function getMovies(searchText) {
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query="' + searchText)
-        .then((response) => {
-            console.log(response);
-            let movies = response.data.Search;
-            let output = '';
-            $.each(movies, (index, movie) => {
-                ouput += `
-                    <div class="col-md-3">
-                        <div class="well text-center">
+function getMovies(searchText){
+    //make request to api using axios
+    // Make a request for a user with a given ID
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=66d439f89eb6b3adc621e575fefa1ed8&language=en-US&query=' + searchText)
+      .then(function (response) {
+          console.log(response);
+        let movies = response.data.results;
+        let output = '';
 
-                        <h5>${movie.Title}</h5>
-                        <a onClick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-                    </div>
-                `;
-            });
-            
-            $('#movies').html(output);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-};
+        $.each(movies, (index, movie) => {
+          output+=`
+            <div class="col-md-3">
+              <div class="well text-center">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+              <h6>${movie.release_date}</h6>
+                <h5>${movie.title}</h5>
+                <small>${movie.vote_average}</small>
+                </br>
+                <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+              </div>
+            </div>
+          `;
+        });
+        $('#movies').html(output);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  function movieSelected(id) {
+      sessionStorage.setItem('movieId', id);
+      window.location = 'movie.html';
+      return false;
+  }
+
+  function getMovie() {
+      let movieId = sessionStorage.getItem('movieId');
+
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=66d439f89eb6b3adc621e575fefa1ed8&language=en-US&query=' + searchText)
+      .then(function (response) {
+          console.log(response);
+        let movies = response.data.results;
+        let output = '';
+
+        $.each(movies, (index, movie) => {
+          output+=`
+            <div class="col-md-3">
+              <div class="well text-center">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+              <h6>${movie.release_date}</h6>
+                <h5>${movie.title}</h5>
+                <small>${movie.vote_average}</small>
+                </br>
+                <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+              </div>
+            </div>
+          `;
+        });
+        $('#movies').html(output);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
